e162f8c662b8999abbca5b091dd5b71c1dfb1674
Kazuki Takahashi <tkhsh.schoen@gmail.com>
2020-10-04 23:15:35 +0900

    add const

--- src/dynamic_cloud_detector.cpp
+++ src/dynamic_cloud_detector.cpp
@@ -34,7 +34,7 @@ DynamicCloudDetector::DynamicCloudDetector(void)
 
 void DynamicCloudDetector::callback(const sensor_msgs::PointCloud2ConstPtr& msg_obstacles_cloud, const nav_msgs::OdometryConstPtr& msg_odom)
 {
-    double start_time = ros::Time::now().toSec();
+    const double start_time = ros::Time::now().toSec();
     static Eigen::Vector2d last_odom_position(msg_odom->pose.pose.position.x, msg_odom->pose.pose.position.y);
     static double last_yaw = tf2::getYaw(msg_odom->pose.pose.orientation);
 
@@ -45,12 +45,12 @@ void DynamicCloudDetector::callback(const sensor_msgs::PointCloud2ConstPtr& msg_
     std::cout << "received cloud size: " << cloud_ptr->points.size() << std::endl;
 
     // transform pointcloud to base frame
-    std::string sensor_frame_id = remove_first_slash(msg_obstacles_cloud->header.frame_id);
-    std::string base_frame_id = remove_first_slash(msg_odom->child_frame_id);
+    const std::string sensor_frame_id = remove_first_slash(msg_obstacles_cloud->header.frame_id);
+    const std::string base_frame_id = remove_first_slash(msg_odom->child_frame_id);
     try{
         geometry_msgs::TransformStamped transform;
         transform = tf_buffer.lookupTransform(base_frame_id, sensor_frame_id, ros::Time(0));
-        Eigen::Matrix4d mat = tf2::transformToEigen(transform.transform).matrix().cast<double>();
+        const Eigen::Matrix4d mat = tf2::transformToEigen(transform.transform).matrix().cast<double>();
         pcl::transformPointCloud(*cloud_ptr, *cloud_ptr, mat);
         cloud_ptr->header.frame_id = base_frame_id;
     }catch(tf2::TransformException& ex){
@@ -59,9 +59,9 @@ void DynamicCloudDetector::callback(const sensor_msgs::PointCloud2ConstPtr& msg_
     }
 
     // transform occupancy grid map
-    Eigen::Vector2d odom_position(msg_odom->pose.pose.position.x, msg_odom->pose.pose.position.y);
-    double yaw = tf2::getYaw(msg_odom->pose.pose.orientation);
-    Eigen::Vector2d diff_odom = Eigen::Rotation2Dd(-last_yaw).toRotationMatrix() * (odom_position - last_odom_position);
+    const Eigen::Vector2d odom_position(msg_odom->pose.pose.position.x, msg_odom->pose.pose.position.y);
+    const double yaw = tf2::getYaw(msg_odom->pose.pose.orientation);
+    const Eigen::Vector2d diff_odom = Eigen::Rotation2Dd(-last_yaw).toRotationMatrix() * (odom_position - last_odom_position);
     double diff_yaw = yaw - last_yaw;
     diff_yaw = atan2(sin(diff_yaw), cos(diff_yaw));
     std::cout << "diff odom: " << diff_odom.transpose() << std::endl;
@@ -95,21 +95,21 @@ void DynamicCloudDetector::input_cloud_to_occupancy_grid_map(const CloudXYZINPtr
 
     // occupancy_grid_map.clear();
     // occupancy_grid_map.resize(GRID_NUM);
-    int cloud_size = cloud_ptr->points.size();
+    const int cloud_size = cloud_ptr->points.size();
     std::vector<bool> obstacle_indices(GRID_NUM, false);
     for(int i=0;i<cloud_size;i++){
-        auto p = cloud_ptr->points[i];
+        const auto& p = cloud_ptr->points[i];
         if(!is_valid_point(p.x, p.y)){
             continue;
         }
         // occupancy_grid_map[get_index_from_xy(p.x, p.y)].add_log_odds(0.01);
-        double distance = sqrt(p.x * p.x + p.y * p.y);
-        double direction = atan2(p.y, p.x);
-        int beam_index = (direction + M_PI) / BEAM_ANGLE_RESOLUTION;
+        const double distance = sqrt(p.x * p.x + p.y * p.y);
+        const double direction = atan2(p.y, p.x);
+        const int beam_index = (direction + M_PI) / BEAM_ANGLE_RESOLUTION;
         if(0 <= beam_index && beam_index < BEAM_NUM){
             beam_list[beam_index] = std::min(beam_list[beam_index], distance);
         }
-        int index = get_index_from_xy(p.x, p.y);
+        const int index = get_index_from_xy(p.x, p.y);
         if(index < 0 || GRID_NUM <= index){
             continue;
         }
@@ -131,9 +131,9 @@ void DynamicCloudDetector::devide_cloud(const CloudXYZINPtr& cloud, CloudXYZINPt
     static_cloud->points.clear();
     for(const auto& pt : cloud->points){
         if(-WIDTH_2 <= pt.x && pt.x <= WIDTH_2 && -WIDTH_2 <= pt.y && pt.y <= WIDTH_2){
-            int index = get_index_from_xy(pt.x, pt.y);
+            const int index = get_index_from_xy(pt.x, pt.y);
             if(0 <= index && index < GRID_NUM){
-                double occupancy = occupancy_grid_map[index].get_occupancy();
+                const double occupancy = occupancy_grid_map[index].get_occupancy();
                 if(occupancy < OCCUPANCY_THRESHOLD){
                     dynamic_cloud->points.push_back(pt);
                 }else{
@@ -146,8 +146,8 @@ void DynamicCloudDetector::devide_cloud(const CloudXYZINPtr& cloud, CloudXYZINPt
 
 int DynamicCloudDetector::get_index_from_xy(const double x, const double y)
 {
-    int _x = floor(x / RESOLUTION + 0.5) + GRID_WIDTH_2;
-    int _y = floor(y / RESOLUTION + 0.5) + GRID_WIDTH_2;
+    const int _x = floor(x / RESOLUTION + 0.5) + GRID_WIDTH_2;
+    const int _y = floor(y / RESOLUTION + 0.5) + GRID_WIDTH_2;
     return _y * GRID_WIDTH + _x;
 }
 
@@ -192,7 +192,7 @@ void DynamicCloudDetector::publish_occupancy_grid_map(const ros::Time& stamp, co
 
 std::string DynamicCloudDetector::remove_first_slash(std::string frame_id)
 {
-    int slash_pos = frame_id.find('/');
+    const int slash_pos = frame_id.find('/');
     if(slash_pos == 0){
         frame_id.erase(0, 1);
     }
@@ -201,7 +201,7 @@ std::string DynamicCloudDetector::remove_first_slash(std::string frame_id)
 
 bool DynamicCloudDetector::is_valid_point(double x, double y)
 {
-    int index = get_index_from_xy(x, y);
+    const int index = get_index_from_xy(x, y);
     if(x < -WIDTH_2 || x > WIDTH_2 || y < -WIDTH_2 || y > WIDTH_2){
         return false;
     }else if(index < 0 || GRID_NUM <= index){
@@ -230,16 +230,16 @@ void DynamicCloudDetector::transform_occupancy_grid_map(const Eigen::Vector2d& t
               s_yaw,  c_yaw, dy_grid,
                   0,      0,        1;
     std::cout << "forward affine:\n" << affine << std::endl;
-    Eigen::Matrix3d affine_inverse = affine.inverse();
+    const Eigen::Matrix3d affine_inverse = affine.inverse();
     std::cout << "reversed affine:\n" << affine_inverse << std::endl;
 
     OccupancyGridMap ogm(GRID_NUM);
-    int show_i = GRID_NUM * 0.5 + GRID_WIDTH - 1;
+    const int show_i = GRID_NUM * 0.5 + GRID_WIDTH - 1;
     for(int i=0;i<GRID_NUM;i++){
         if(i == show_i)
             std::cout << "i: " << i << std::endl;
-        double x_i = get_x_index_from_index(i) - GRID_WIDTH_2;
-        double y_i = get_y_index_from_index(i) - GRID_WIDTH_2;
+        const double x_i = get_x_index_from_index(i) - GRID_WIDTH_2;
+        const double y_i = get_y_index_from_index(i) - GRID_WIDTH_2;
         Eigen::Vector3d ogm_i(x_i, y_i, 1);
         if(i == show_i)
             std::cout << "ogm_i.transpose(): " << ogm_i.transpose() << std::endl;
@@ -252,10 +252,10 @@ void DynamicCloudDetector::transform_occupancy_grid_map(const Eigen::Vector2d& t
             std::cout << map_i(0) * RESOLUTION << ", " << map_i(1) * RESOLUTION << std::endl;
 
         // bilinear interpolation
-        int x_0 = std::floor(map_i(0));
-        int x_1 = x_0 + 1;
-        int y_0 = std::floor(map_i(1));
-        int y_1 = y_0 + 1;
+        const int x_0 = std::floor(map_i(0));
+        const int x_1 = x_0 + 1;
+        const int y_0 = std::floor(map_i(1));
+        const int y_1 = y_0 + 1;
         if(i == show_i)
             std::cout << x_0 << ", " << x_1 << ", " << y_0 << ", " << y_1 << std::endl;
         if(x_0 <= -GRID_WIDTH_2 || GRID_WIDTH_2 <= x_1){
@@ -264,22 +264,22 @@ void DynamicCloudDetector::transform_occupancy_grid_map(const Eigen::Vector2d& t
         if(y_0 <= -GRID_WIDTH_2 || GRID_WIDTH_2 <= y_1){
             continue;
         }
-        int index_0_0 = (y_0 + GRID_WIDTH_2) * GRID_WIDTH + x_0 + GRID_WIDTH_2;
-        int index_0_1 = (y_1 + GRID_WIDTH_2) * GRID_WIDTH + x_0 + GRID_WIDTH_2;
-        int index_1_0 = (y_0 + GRID_WIDTH_2) * GRID_WIDTH + x_1 + GRID_WIDTH_2;
-        int index_1_1 = (y_1 + GRID_WIDTH_2) * GRID_WIDTH + x_1 + GRID_WIDTH_2;
+        const int index_0_0 = (y_0 + GRID_WIDTH_2) * GRID_WIDTH + x_0 + GRID_WIDTH_2;
+        const int index_0_1 = (y_1 + GRID_WIDTH_2) * GRID_WIDTH + x_0 + GRID_WIDTH_2;
+        const int index_1_0 = (y_0 + GRID_WIDTH_2) * GRID_WIDTH + x_1 + GRID_WIDTH_2;
+        const int index_1_1 = (y_1 + GRID_WIDTH_2) * GRID_WIDTH + x_1 + GRID_WIDTH_2;
         if(i == show_i)
             std::cout << index_0_0 << ", " << index_0_1 << ", " << index_1_0 << ", " << index_1_1 << std::endl;
 
-        Eigen::Vector2d y_vec(y_1 - map_i(1), map_i(1) - y_0);
-        Eigen::Vector2d x_vec(x_1 - map_i(0), map_i(0) - x_0);
+        const Eigen::Vector2d y_vec(y_1 - map_i(1), map_i(1) - y_0);
+        const Eigen::Vector2d x_vec(x_1 - map_i(0), map_i(0) - x_0);
         Eigen::Matrix2d value_mat;
         // value_mat << map[index_0_0].get_log_odds(), map[index_1_0].get_log_odds(),
         //              map[index_0_1].get_log_odds(), map[index_1_1].get_log_odds();
         value_mat << map[index_0_0].get_occupancy(), map[index_1_0].get_occupancy(),
                      map[index_0_1].get_occupancy(), map[index_1_1].get_occupancy();
 
-        double ogm_value = y_vec.transpose() * value_mat * x_vec;
+        const double ogm_value = y_vec.transpose() * value_mat * x_vec;
         ogm[i].log_odds = std::log(ogm_value / (1 - ogm_value));
         if(i == show_i)
             std::cout << "\033[31m" << ogm_value << "\033[0m" << std::endl;
@@ -295,13 +295,13 @@ void DynamicCloudDetector::set_clear_grid_cells(const std::vector<double>& beam_
         double direction = i * BEAM_ANGLE_RESOLUTION - M_PI;
         direction = atan2(sin(direction), cos(direction));
         // std::cout << i << ": " << direction << ", " << beam_list[i] << std::endl;
-        double c = cos(direction);
-        double s = sin(direction);
+        const double c = cos(direction);
+        const double s = sin(direction);
         for(double range=0.0;range<beam_list[i];range+=RESOLUTION){
-            double x = range * c;
-            double y = range * s;
+            const double x = range * c;
+            const double y = range * s;
             if(is_valid_point(x, y)){
-                int index = get_index_from_xy(x, y);
+                const int index = get_index_from_xy(x, y);
                 if(!obstacle_indices[index]){
                     map[index].add_log_odds(-0.2);
                 }else{
